694b6dc7c2e3a0358aff2f1baead0b15de4bb6f2
Myrat <myratgeldigurbangeldiyew09@gmail.com>
2026-08-11 05:18:52 +0500

    Add Hello World and basic operations in Python

---
+++ English/01_Day_Introduction/helloworld.py
@@ -0,0 +1,23 @@
+# Introduction
+# Day 1 - 30DaysOfPython Challenge
+
+print("Hello World!")  # Print Hello World
+
+# Arithmetic operations
+print(2 + 3)   # Addition(+)
+print(3 - 1)   # Subtraction(-)
+print(2 * 3)   # Multiplication(*)
+print(3 / 2)   # Division(/)
+print(3 ** 2)  # Exponential(**)
+print(3 % 2)   # Modulus(%)
+print(3 // 2)  # Floor division operator(//)
+
+# Checking data types
+print(type(10))                  # Int
+print(type(3.14))                # Float
+print(type(1 + 3j))              # Complex
+print(type('Myrat'))             # String
+print(type([1, 2, 3]))           # List
+print(type({'name': 'Myrat'}))   # Dictionary
+print(type({9.8, 3.14, 2.7}))    # Set
+print(type((9.8, 3.14, 2.7)))    # Tuple
