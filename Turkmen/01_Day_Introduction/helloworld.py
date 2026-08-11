c736943e82a3c99b21fa13c327671a5441e5f4bb
Myrat <myratgeldigurbangeldiyew09@gmail.com>
2026-08-11 05:13:50 +0500

    Add hello world and basic math operations in Python

---
+++ Turkmen/01_Day_Introduction/helloworld.py
@@ -0,0 +1,23 @@
+# Giriş
+# 1-nji Gün - 30DaysOfPython Çagryşy
+
+print("Salam Dünýä!")  # Salam Dünýä diýip çykarmak
+
+# Matematiki amallar
+print(2 + 3)   # Goşmak (+)
+print(3 - 1)   # Aýyrmak (-)
+print(2 * 3)   # Köpeltmek (*)
+print(3 / 2)   # Bölmek (/)
+print(3 ** 2)  # Derejä getirmek (**)
+print(3 % 2)   # Bölümden galan galyndy (%)
+print(3 // 2)  # Bütin bölmek (Floor division)
+
+# Maglumat görnüşlerini barlamak (Data types)
+print(type(10))                  # Bütin san (Int)
+print(type(3.14))                # Onluk san (Float)
+print(type(1 + 3j))              # Kompleks san (Complex)
+print(type('Myrat'))             # Metin / Ýazgy (String)
+print(type([1, 2, 3]))           # Sanaw (List)
+print(type({'name': 'Myrat'}))   # Sözlük (Dictionary)
+print(type({9.8, 3.14, 2.7}))    # Toplum (Set)
+print(type((9.8, 3.14, 2.7)))    # Üýtgetmesiz sanaw (Tuple)
