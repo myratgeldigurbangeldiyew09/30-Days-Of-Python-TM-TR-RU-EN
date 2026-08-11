fc122ebc40e6fb0354f8cb44fbce5e1d4d16c00b
Myrat <myratgeldigurbangeldiyew09@gmail.com>
2026-08-11 05:16:03 +0500

    Add introductory Python script for 30DaysOfPython

---
+++ Turkish/01_Day_Introduction/helloworld.py
@@ -0,0 +1,23 @@
+# Giriş
+# 1. Gün - 30DaysOfPython Meydan Okuması
+
+print("Merhaba Dünya!")  # Ekrana Merhaba Dünya yazdırır
+
+# Matematiksel İşlemler
+print(2 + 3)   # Toplama (+)
+print(3 - 1)   # Çıkarma (-)
+print(2 * 3)   # Çarpma (*)
+print(3 / 2)   # Bölme (/)
+print(3 ** 2)  # Üs alma (**)
+print(3 % 2)   # Mod alma / Kalan (%)
+print(3 // 2)  # Tam bölme (Floor division)
+
+# Veri Tiplerini Kontrol Etme (Data types)
+print(type(10))                  # Tam sayı (Int)
+print(type(3.14))                # Ondalıklı sayı (Float)
+print(type(1 + 3j))              # Karmaşık sayı (Complex)
+print(type('Myrat'))             # Metin (String)
+print(type([1, 2, 3]))           # Liste (List)
+print(type({'name': 'Myrat'}))   # Sözlük (Dictionary)
+print(type({9.8, 3.14, 2.7}))    # Küme (Set)
+print(type((9.8, 3.14, 2.7)))    # Demet (Tuple)
